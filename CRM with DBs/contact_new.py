7aca4b47b1fcfcd5cb744888ccd2b1e5b4c3ee04
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-02-26 16:32:14 -0500

    DB changes

--- CRM with DBs/contact_new.py
+++ CRM with DBs/contact_new.py
@@ -1,4 +1,4 @@
-from peewee import SqliteDatabase, Model
+from peewee import SqliteDatabase, Model, CharField, TextField
 
 db = SqliteDatabase('crm.db')
 
@@ -15,18 +15,6 @@ class Contact(Model):
     def full_name(self):
         return "{} {}".format(self.first_name, self.last_name)
 
-    # def __str__(self):
-    #     return "Name: {} {}, email: {}, note: {}".format(self.first_name, self.last_name, self.email, self.note)
-
 
 db.connect()
 db.create_tables([Contact])
-# test1 = Contact.create("Sanchit", "Jain", "sanchit.jain@mail.mcgill.ca", "hello")
-# test2 = Contact.create("Tarishi", "Jain", "smartaru1997@gmail.com", "hello")
-# test3 = Contact.create("Josh", "Teneycke", "james.mcgill@mail.mcgill.ca", "hello")
-# print(len(Contact.contacts))
-# test1.full_name()
-# test1.update()
-# Contact.all()
-# # Contact.delete_all()
-# print(len(Contact.contacts))
