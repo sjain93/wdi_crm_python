423a14d1ea4638e77ff9678fb2617618dd397b43
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-02-26 23:23:24 -0500

    final changes to CRM assignment, db ready

--- CRM with DBs/crm_new.py
+++ CRM with DBs/crm_new.py
@@ -50,32 +50,61 @@ class CRM:
 
     def modify_existing_contact(self):
         print("Please select the id of the contact you would like to modify")
-        for contact in Contact.contacts:
+        for contact in Contact.select():
             print("{} {} {}".format(contact.id, contact.first_name, contact.email))
         input_id = int(input())
         contact_detail = Contact.get(id=input_id)
-        Contact.update(contact_detail)
-        # print(contact_detail)
+        # Contact.update(contact_detail)
+        print("Copy and paste the attribute would you like to modify:\nfirst_name\nlast_name\nemail\nnote")
+        mod_field = input()
+        print("What would you like to change {} to?".format(mod_field))
+        new_val = input()
+        if mod_field == "first_name":
+            contact_detail.first_name = new_val
+            contact_detail.save()
+        elif mod_field == "last_name":
+            contact_detail.last_name = new_val
+            contact_detail.save()
+        elif mod_field == "email":
+            contact_detail.email = new_val
+            contact_detail.save()
+        elif mod_field == "note":
+            contact_detail.note = new_val
+            contact_detail.save()
+        print("Contact updated")
 
     def delete_contact(self):
         print("Please select the id of the contact you would like to delete")
-        for contact in Contact.contacts:
+        for contact in Contact.select():
             print("{} {} {}".format(contact.id, contact.first_name, contact.email))
         input_id = int(input())
         contact_detail = Contact.get(id=input_id)
-        Contact.delete(contact_detail)
+        contact_detail.delete_instance()
+        print("contact deleted")
 
     def display_all_contacts(self):
-        Contact.all()
+        for contact in Contact.select():
+            print("{} {} {} {}".format(contact.id, contact.first_name, contact.email, contact.note))
 
     def search_by_attribute(self):
         print("Copy and paste the attribute would you like to search by:\nfirst_name\nlast_name\nemail\nnote")
         search_field = input()
         print("What value would you like to search by?")
         search_value = input()
-        x = Contact.find_by(search_field, search_value)
-        print(x)
-
+        if search_field == "first_name":
+            result = Contact.get(Contact.first_name == search_value)
+        elif search_field == "last_name":
+            result = Contact.get(Contact.last_name == search_value)
+        elif search_field == "email":
+            result = Contact.get(Contact.email == search_value)
+        elif search_field == "note":
+            result = Contact.get(Contact.note == search_value)
+        else:
+            print("Incorrect query")
+        if result:
+            print("Your contact:{} {}, email: {}, note {}".format(result.first_name, result.last_name, result.email, result.note))
+        else:
+            print("Incorrect query")
 
 a_crm_app = CRM()
 a_crm_app.main_menu()
